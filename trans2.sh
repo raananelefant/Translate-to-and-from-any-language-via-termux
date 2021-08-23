38d35c44867e7e50492d262be784437ececd1935
raananelefant <58908387+raananelefant@users.noreply.github.com>
2021-08-23 14:11:39 +0300

    Update trans2.sh

--- trans2.sh
+++ trans2.sh
@@ -1,3 +1,4 @@
-trans :"$1" -b "${@:2}" > file
-termux-share -c "text/plain" -a send < file
+trans :"$1" -b -p "${@:2}" #Recites translation
+trans :"$1" -b "${@:2}" > file #Writes translation to 'file'
+termux-share -c "text/plain" -a send < file #Share translated text
 rm file
